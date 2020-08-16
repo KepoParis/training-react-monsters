fd8e8e4e7f3006d4022e5efc49bcdffa2d6b2387
Kevin Powell Noumbissie <kevin-powell@hotmail.fr>
2020-08-16 14:43:40 +0200

    Updates

---
+++ precache-manifest.558a7aff58ee98ff9446f6fda68e761d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6bb497e6115fa19c137589d096f4c3",
+    "url": "/training-react-monsters/index.html"
+  },
+  {
+    "revision": "7ceac9778fe82ddea8fe",
+    "url": "/training-react-monsters/static/css/main.895e3d17.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/training-react-monsters/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/training-react-monsters/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ceac9778fe82ddea8fe",
+    "url": "/training-react-monsters/static/js/main.a0e80da3.chunk.js"
+  },
+  {
+    "revision": "61a4f132b573ab846a08",
+    "url": "/training-react-monsters/static/js/runtime-main.b7470745.js"
+  }
+]);
